8b6f2e1a45a41e65c294628da39ddb56c7440dfb
qinchunabng <373413704@qq.com>
2022-11-13 19:23:44 +0800

    add lib package usage

--- stack/main.c
+++ stack/main.c
@@ -19,6 +19,40 @@ void print_s(const void *r)
   printf("%d %s %d %d\n", ptr->id, ptr->name, ptr->chinese, ptr->math);
 }
 
+/**
+ * 1.静态库
+ * 1) libxxx.a
+ *    xx指代库名
+ * 2) ar -cr libxxx.a yyy.o
+ *    将.o文件打包为静态库文件
+ * 3) 发布到
+ *    /usr/local/include
+ *    /usr/local/lib
+ *    头文件放到/usr/local/include目录，静态库文件放到/usr/local/lib
+ * 4) gcc -L/user/local/lib -o main main.o -lxx
+ *    -l 参数必须在最后，有依赖
+ *
+ * 2.动态库
+ * 1) libxx.so
+ *    动态库以.so结尾
+ *
+ * 2) gcc -shared -fpic -o libxx.so yyy.c
+ *    将.c打包为动态库
+ * 3) 发布到
+ *    /usr/local/include
+ *    /usr/local/lib
+ *    头文件放到/usr/local/include目录，静态库文件放到/usr/local/lib
+ *    非root用户发布
+ *    cp xx.so ~/lib
+ *    export LD_LIBRARY_PATH=~/lib
+ * 4) 在/etc/ld.so.conf中添加路径
+ *    /sbin/config 重读/etc/ld.so.conf
+ *
+ *
+ * 3. 查看动态库依赖
+ * ldd - print shared library dependencies
+ *
+ */
 int main(void)
 {
   int i, ret;
